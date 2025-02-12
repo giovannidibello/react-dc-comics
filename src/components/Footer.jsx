2c337229c6070c45e26d903b9808adc0fdfb2de4
Giovanni <dibellogio@gmail.com>
2025-02-12 16:31:56 +0100

    add html in function Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,76 @@
 // Footer.jsx
 export default function Footer() {
-    return (<footer> /* contenuto qui */ </footer>);
+    return (
+
+        <footer>
+
+            <div class="footer-sections">
+
+                <div class="footer-column">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+
+                </div>
+
+
+                <div class="footer-column">
+                    <h3>DC</h3>
+                    <ul>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-column">
+                    <h3>SITES</h3>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+
+
+            </div>
+
+
+            <div class="footer-bottom">
+                <button>SIGN-UP NOW!</button>
+                <div class="social-links">
+                    <span>FOLLOW US</span>
+                    <a href="#"><img src="/img/footer-facebook.png" alt="Facebook" /></a>
+                    <a href="#"><img src="/img/footer-twitter.png" alt="Twitter" /></a>
+                    <a href="#"><img src="/img/footer-youtube.png" alt="YouTube" /></a>
+                    <a href="#"><img src="/img/footer-pinterest.png" alt="Pinterest" /></a>
+                    <a href="#"><img src="/img/footer-periscope.png" alt="Periscope" /></a>
+                </div>
+            </div>
+        </footer>
+
+    );
 }
