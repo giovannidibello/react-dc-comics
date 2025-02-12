7f22fdfd9ce2b5ed69d62198ca70ae2322274cb3
Giovanni <dibellogio@gmail.com>
2025-02-12 15:57:07 +0100

    add html in function Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,30 @@
+// Main.jsx
+export default function Main() {
+    return (
+        <main>
+
+            <section className="content">
+                <h1>Content goes here</h1>
+            </section>
+
+            <section className="blue-strip">
+                <div className="icon">
+                    <a href="#"><img src="./img/buy-comics-digital-comics.png" alt="Digital Comics" /> Digital Comics</a>
+                </div>
+                <div className="icon">
+                    <a href="#"><img src="./img/buy-comics-merchandise.png" alt="DC Merchandise" /> DC Merchandise</a>
+                </div>
+                <div className="icon">
+                    <a href="#"><img src="./img/buy-comics-subscriptions.png" alt="Subscription" /> Subscription</a>
+                </div>
+                <div className="icon">
+                    <a href="#"><img src="./img/buy-comics-shop-locator.png" alt="Comic Shop Locator" /> Comic Shop Locator</a>
+                </div>
+                <div className="icon">
+                    <a href="#"><img src="./img/buy-dc-power-visa.svg" alt="DC Power Visa" /> DC Power Visa</a>
+                </div>
+            </section>
+
+        </main>
+    );
+}
